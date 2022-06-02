6adbfb22a3578a1f66b72b9ef9fe0430df23c0af
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2022-06-02 14:00:00 +0300

    Handle errors when getting schedule for a worker

--- src/api/schedule.ts
+++ src/api/schedule.ts
@@ -13,13 +13,25 @@ export async function getScheduleForEmployee(
 ): Promise<Schedule> {
     const day = getDayAsIso(scheduleDay)
     const url = `https://www.varaaheti.fi/groom/fi/api/public/locations/${locationId}/views/palvelut/services/${serviceId}/available?worker_id=${workerId}&date=${day}`
-    const response = await axios.get<response>(url)
+    try {
+        const response = await axios.get<response>(url)
 
-    const scheduleForGivenDay = response.data.data.find((schedule) => schedule.date === day)
-    if (!scheduleForGivenDay)
-        throw new Error(`Could not find schedule for worker ${workerId} and location ${locationId}`)
+        return findOrThrow(response.data.data, (schedule) => schedule.date === day)
+    } catch (e) {
+        console.log(`Failed to get schedule for worker ${workerId}, location ${locationId} and service ${serviceId}`)
 
-    return scheduleForGivenDay
+        return {
+            date: day,
+            available: [],
+        }
+    }
+}
+
+function findOrThrow<T>(collection: T[], predicate: (value: T, index: number, obj: T[]) => unknown, thisArg?: any): T {
+    const result = collection.find(predicate)
+    if (!result) throw new Error('Result not found')
+
+    return result
 }
 
 export interface Schedule {
