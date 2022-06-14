ba5d45483917670fb16467e55bbb3bb01a61ba4e
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2022-06-14 22:53:14 +0300

    moved person styles to a component

--- src/Person.tsx
+++ src/Person.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Schedule } from './Schedule'
 import { workerWithSchedule } from './api/workers'
+import styled from 'styled-components'
 
 function firstName(name: string) {
     return name.split(' ')[0]
@@ -8,16 +9,42 @@ function firstName(name: string) {
 
 export function Person(worker: workerWithSchedule) {
     return (
-        <div className="person">
-            <div className="name">
+        <PersonContainer className="person">
+            <Name className="name">
                 <div>{firstName(worker.name)}</div>
                 <div className="location">{worker.location.name}</div>
-            </div>
-            <div className="image">
+            </Name>
+            <Image className="image">
                 <img src={worker.image_url} />
-            </div>
+            </Image>
 
             <Schedule {...worker.schedule} />
-        </div>
+        </PersonContainer>
     )
 }
+
+const PersonContainer = styled.div`
+    display: flex;
+    height: 400px;
+    width: 100%;
+    flex: 1;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+
+    @media only screen and (min-width: 1001px) {
+        flex-direction: row;
+    }
+`
+const Name = styled.div`
+    margin-top: 10px;
+
+    @media only screen and (min-width: 1001px) {
+        width: 15%;
+    }
+`
+const Image = styled.div`
+    @media only screen and (min-width: 1001px) {
+        width: 25%;
+    }
+`
